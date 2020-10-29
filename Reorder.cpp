3f7a05529e574f420c7343db98f529e8135eb7d2
Zeus2k01 <kayoze2006@gmail.com>
2020-10-29 10:33:20 +0530

    Create Reorder.cpp

---
+++ Reorder.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=a[i];
+        }
+        if(sum==m)
+        {
+            cout<<"YES \n";
+        }
+        else
+        cout<<"NO \n";
+        
+    }
+}

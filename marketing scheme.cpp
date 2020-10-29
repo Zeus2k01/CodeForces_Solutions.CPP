887ba2925ae30c73a8b4c0416b0f7e55909e4a38
Zeus2k01 <kayoze2006@gmail.com>
2020-10-29 10:33:00 +0530

    Create marketing scheme.cpp

---
+++ marketing scheme.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int l,r;
+        cin>>l>>r;
+        if(2*l<=r)
+        {
+            cout<<"NO \n";
+            
+        }
+        else if(2*l>r)
+        
+        {
+            cout<<"YES \n";
+        }
+        
+    }
+}
